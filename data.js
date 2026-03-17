cde199d124d3cc6806c86507466d5a190c1dd12a
nicholasd-art <nicholas.deeney@education.nsw.gov.au>
2026-03-17 21:47:21 +1100

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+window.MORPHEMES = {
+  bases: [
+    "act","form","appear","design","play","read","write","build"
+  ],
+  prefixes: [
+    "re-","un-","pre-","non-","dis-"
+  ],
+  suffixes: [
+    "-ing","-ed","-er","-able","-less"
+  ]
+};
